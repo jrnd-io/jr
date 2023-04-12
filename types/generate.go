54ded1c55cd9c04e6a010a1548c6767f0a0eeddf
ugol <ugo.landini@gmail.com>
2023-04-12 09:56:32 +0200

    fixed gogen-avro path

--- types/generate.go
+++ types/generate.go
@@ -1,4 +1,4 @@
 package types
 
-//go:generate gogen-avro -package types . NetDevice.avsc
-//go:generate gogen-avro -package types . User.avsc
+//go:generate $GOPATH/bin/gogen-avro -package types . NetDevice.avsc
+//go:generate $GOPATH/bin/gogen-avro -package types . User.avsc

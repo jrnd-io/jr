53b493af15b93074be8f4ec5d33da4808af23485
Ugo Landini <ugo.landini@gmail.com>
2023-04-14 02:14:13 +0200

    experiments with go:generate

--- types/generate.go
+++ types/generate.go
@@ -2,3 +2,5 @@ package types
 
 //go:generate $GOPATH/bin/gogen-avro -package types . NetDevice.avsc
 //go:generate $GOPATH/bin/gogen-avro -package types . User.avsc
+//go:generate go run instance.go NetDevice
+//go:generate go run instance.go User

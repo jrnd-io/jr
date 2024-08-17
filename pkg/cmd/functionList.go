c6e006671d9bb36973756cd27e24e7b406073906
ugol <ugo.landini@gmail.com>
2024-08-18 01:53:03 +0200

    added sorted output, total number of functions in category and 4 missing function descriptions

--- pkg/cmd/functionList.go
+++ pkg/cmd/functionList.go
@@ -26,6 +26,7 @@ import (
 	"github.com/ugol/jr/pkg/functions"
 	"os"
 	"os/exec"
+	"slices"
 	"strings"
 )
 
@@ -57,17 +58,21 @@ func doList(cmd *cobra.Command, args []string) {
 	noColor, _ := cmd.Flags().GetBool("nocolor")
 
 	if category && len(args) > 0 {
+		var functionNames []string
 		for k, v := range functions.DescriptionMap() {
 			if strings.Contains(v.Category, args[0]) {
-				printFunction(k, isMarkdown, noColor)
+				functionNames = append(functionNames, k)
 			}
 		}
+		sortAndPrint(functionNames, isMarkdown, noColor)
 	} else if find && len(args) > 0 {
+		var functionNames []string
 		for k, v := range functions.DescriptionMap() {
 			if strings.Contains(v.Description, args[0]) || strings.Contains(v.Name, args[0]) {
-				printFunction(k, isMarkdown, noColor)
+				functionNames = append(functionNames, k)
 			}
 		}
+		sortAndPrint(functionNames, isMarkdown, noColor)
 	} else if len(args) == 1 {
 
 		if run {
@@ -83,15 +88,30 @@ func doList(cmd *cobra.Command, args []string) {
 			printFunction(args[0], isMarkdown, noColor)
 		}
 	} else {
-		count := 0
-		for k := range functions.FunctionsMap() {
-			count++
-			printFunction(k, isMarkdown, noColor)
+
+		//l := len(functions.FunctionsMap())
+		l := len(functions.DescriptionMap())
+		functionNames := make([]string, l)
+
+		i := 0
+		for k := range functions.DescriptionMap() {
+			functionNames[i] = k
+			i++
 		}
-		fmt.Println()
-		fmt.Printf("Total functions: %d\n", count)
+		sortAndPrint(functionNames, isMarkdown, noColor)
+
 	}
 	fmt.Println()
+}
+
+func sortAndPrint(functionNames []string, isMarkdown bool, noColor bool) {
+	slices.Sort(functionNames)
+	for _, k := range functionNames {
+		printFunction(k, isMarkdown, noColor)
+		//fmt.Println(k)
+	}
+	fmt.Println()
+	fmt.Printf("Total functions: %d\n", len(functionNames))
 }
 
 func printFunction(name string, isMarkdown bool, noColor bool) (functions.FunctionDescription, bool) {
@@ -104,21 +124,8 @@ func printFunction(name string, isMarkdown bool, noColor bool) (functions.Functi
 		Reset = "\033[0m"
 	}
 
-	if !isMarkdown {
-
-		if found {
-			fmt.Println()
-			fmt.Printf("%sName: %s%s\n", Cyan, Reset, f.Name)
-			fmt.Printf("%sCategory: %s%s\n", Cyan, Reset, f.Category)
-			fmt.Printf("%sDescription: %s%s\n", Cyan, Reset, f.Description)
-			fmt.Printf("%sParameters: %s%s\n", Cyan, Reset, f.Parameters)
-			fmt.Printf("%sLocalizable: %s%v\n", Cyan, Reset, f.Localizable)
-			fmt.Printf("%sReturn: %s%s\n", Cyan, Reset, f.Return)
-			fmt.Printf("%sExample: %s%s\n", Cyan, Reset, f.Example)
-			fmt.Printf("%sOutput: %s%s\n", Cyan, Reset, f.Output)
-		}
-	} else {
-		if found {
+	if found {
+		if isMarkdown {
 			fmt.Println()
 			fmt.Printf("## Name: %s \n", f.Name)
 			fmt.Printf("**Category:** %s\\\n", f.Category)
@@ -133,6 +140,16 @@ func printFunction(name string, isMarkdown bool, noColor bool) (functions.Functi
 			fmt.Printf("**Return:** `%s`\\\n", f.Return)
 			fmt.Printf("**Example:** `%s`\\\n", f.Example)
 			fmt.Printf("**Output:** `%s`\n", f.Output)
+		} else {
+			fmt.Println()
+			fmt.Printf("%sName: %s%s\n", Cyan, Reset, f.Name)
+			fmt.Printf("%sCategory: %s%s\n", Cyan, Reset, f.Category)
+			fmt.Printf("%sDescription: %s%s\n", Cyan, Reset, f.Description)
+			fmt.Printf("%sParameters: %s%s\n", Cyan, Reset, f.Parameters)
+			fmt.Printf("%sLocalizable: %s%v\n", Cyan, Reset, f.Localizable)
+			fmt.Printf("%sReturn: %s%s\n", Cyan, Reset, f.Return)
+			fmt.Printf("%sExample: %s%s\n", Cyan, Reset, f.Example)
+			fmt.Printf("%sOutput: %s%s\n", Cyan, Reset, f.Output)
 		}
 	}
 	return f, found

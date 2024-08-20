52fb697c6b1571519d3ee6f7fd4d15b53ad81256
ugol <ugo.landini@gmail.com>
2024-08-20 15:36:31 +0200

    updated license

--- pkg/producers/azblobstorage/config.go
+++ pkg/producers/azblobstorage/config.go
@@ -1,4 +1,4 @@
-// Copyright © 2022 Vincenzo Marchese <vincenzo.marchese@gmail.com>
+// Copyright © 2024 JR team
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -17,6 +17,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 package azblobstorage
 
 type Container struct {
--- pkg/producers/s3/s3Producer.go
+++ pkg/producers/s3/s3Producer.go
@@ -1,15 +1,35 @@
+// Copyright © 2024 JR team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package s3
 
 import (
 	"bytes"
 	"encoding/json"
-	"os"
-        "strings"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/google/uuid"
 	"github.com/rs/zerolog/log"
+	"os"
+	"strings"
 )
 
 type Config struct {
@@ -58,8 +78,7 @@ func (p *S3Producer) Produce(k []byte, v []byte, o any) {
 		key = string(k)
 	}
 
-
-        //object will be stored with no content type 
+	//object will be stored with no content type
 	_, err := p.client.PutObject(&s3.PutObjectInput{
 		Body:   bytes.NewReader(v),
 		Bucket: aws.String(bucket),
